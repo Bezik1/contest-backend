ee26c86d9b9c3d238e3a36e6756b2f6b4bb05f79
Bezik1 <mateusz.adamowicz.lo1@gmail.com>
2022-10-19 20:11:39 +0200

    Adding frontend to backend and deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AnnouncementsModule } from './announcements/announcements.module';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'views'),
+    }),
     MongooseModule.forRoot(process.env.API_LINK),
     UsersModule,
     AnnouncementsModule, 
